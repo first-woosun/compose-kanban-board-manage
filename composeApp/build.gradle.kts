6d05a7dedcff0b34d3a06a4dd558a09192be3d51
First_woosun <dbdntjs620@gmail.com>
2026-03-30 21:20:17 +0900

    fix: ktlint 실패 수정

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
 ktlint {
     filter {
         exclude { element ->
-            val path = element.file.path
+            val path = element.file.path.replace("\\", "/")
             path.contains("/build/") ||
                 (!path.contains("/src/commonMain/") && !path.contains("/src/commonTest/"))
         }
