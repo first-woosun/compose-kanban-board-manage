5700a318ec1e18d0b6035bba406ffbd78d8db4d8
First_woosun <dbdntjs620@gmail.com>
2026-03-30 21:36:02 +0900

    style: 불필요한 개행 제거

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/card/model/TitleTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/card/model/TitleTest.kt
@@ -6,17 +6,16 @@ import org.junit.Assert
 import org.junit.Test
 import woowacourse.kanban.domain.task.Title
 
-@OptIn(ExperimentalTestApi::class)
 class TitleTest {
 
     @Test
     fun `제목이 비어있지 않고 공백이지도 않은 경우 오류가 발생하지 않아야 한다`() {
         // given
-        val title =
-            "제목"
+        val title = "제목"
+
         // when
-        val data =
-            Title(title)
+        val data = Title(title)
+
         // then
         assertEquals(
             "제목",
@@ -27,9 +26,8 @@ class TitleTest {
     @Test
     fun `제목이 비어 있는 경우 오류가 발생해야 한다`() {
         // given
-        val title =
-            ""
-        // when
+        val title = ""
+
         // then
         Assert.assertThrows(IllegalArgumentException::class.java) {
             Title(title)
@@ -39,9 +37,8 @@ class TitleTest {
     @Test
     fun `제목이 공백으로만 이루어져 있는 경우 오류가 발생해야 한다`() {
         // given
-        val title =
-            " "
-        // when
+        val title = " "
+
         // then
         Assert.assertThrows(IllegalArgumentException::class.java) {
             Title(title)
