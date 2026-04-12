4d17f223869bafa80c7f62278999e59d3ce5934b
First_woosun <dbdntjs620@gmail.com>
2026-04-12 21:10:52 +0900

    style: ktlintformat 적용

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/card/model/TaskRulesTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/card/model/TaskRulesTest.kt
@@ -10,26 +10,25 @@ import woowacourse.kanban.domain.task.Review
 import woowacourse.kanban.domain.task.TaskRules
 import woowacourse.kanban.domain.task.TaskStatus
 import woowacourse.kanban.domain.task.Todo
-import kotlin.test.assertEquals
 
 class TaskRulesTest {
     @Test
-    fun `todo의 isDeletable은 true다` () {
-        val taskRules : TaskRules = Todo()
+    fun `todo의 isDeletable은 true다`() {
+        val taskRules: TaskRules = Todo()
 
         assertTrue(taskRules.isDeletable)
     }
 
     @Test
-    fun `todo는 inProgress로 이동할 수 있다` () {
-        val taskRules : TaskRules = Todo()
+    fun `todo는 inProgress로 이동할 수 있다`() {
+        val taskRules: TaskRules = Todo()
 
         assertTrue(taskRules.moveTo(TaskStatus.IN_PROGRESS) is InProgress)
     }
 
     @Test
-    fun `todo는 review로 이동하면 예외가 발생한다` () {
-        val taskRules : TaskRules = Todo()
+    fun `todo는 review로 이동하면 예외가 발생한다`() {
+        val taskRules: TaskRules = Todo()
 
         assertThrows(IllegalStateException::class.java) {
             taskRules.moveTo(TaskStatus.REVIEW)
@@ -37,8 +36,8 @@ class TaskRulesTest {
     }
 
     @Test
-    fun `todo는 done으로 이동하면 예외가 발생한다` () {
-        val taskRules : TaskRules = Todo()
+    fun `todo는 done으로 이동하면 예외가 발생한다`() {
+        val taskRules: TaskRules = Todo()
 
         assertThrows(IllegalStateException::class.java) {
             taskRules.moveTo(TaskStatus.DONE)
@@ -46,36 +45,36 @@ class TaskRulesTest {
     }
 
     @Test
-    fun `todo의 requireAssignee는 false다` () {
-        val taskRules : TaskRules = Todo()
+    fun `todo의 requireAssignee는 false다`() {
+        val taskRules: TaskRules = Todo()
 
         assertFalse(taskRules.requireAssignee)
     }
 
     @Test
-    fun `inProgress의 isDeletable은 true다` () {
-        val taskRules : TaskRules = InProgress()
+    fun `inProgress의 isDeletable은 true다`() {
+        val taskRules: TaskRules = InProgress()
 
         assertTrue(taskRules.isDeletable)
     }
 
     @Test
-    fun `inProgress는 todo로 이동할 수 있다` () {
-        val taskRules : TaskRules = InProgress()
+    fun `inProgress는 todo로 이동할 수 있다`() {
+        val taskRules: TaskRules = InProgress()
 
         assertTrue(taskRules.moveTo(TaskStatus.TO_DO) is Todo)
     }
 
     @Test
-    fun `inProgress는 review로 이동할 수 있다` () {
-        val taskRules : TaskRules = InProgress()
+    fun `inProgress는 review로 이동할 수 있다`() {
+        val taskRules: TaskRules = InProgress()
 
         assertTrue(taskRules.moveTo(TaskStatus.REVIEW) is Review)
     }
 
     @Test
-    fun `inProgress는 done으로 이동하면 예외가 발생한다` () {
-        val taskRules : TaskRules = InProgress()
+    fun `inProgress는 done으로 이동하면 예외가 발생한다`() {
+        val taskRules: TaskRules = InProgress()
 
         assertThrows(IllegalStateException::class.java) {
             taskRules.moveTo(TaskStatus.DONE)
@@ -83,22 +82,22 @@ class TaskRulesTest {
     }
 
     @Test
-    fun `inProgress의 requireAssignee는 true다` () {
-        val taskRules : TaskRules = InProgress()
+    fun `inProgress의 requireAssignee는 true다`() {
+        val taskRules: TaskRules = InProgress()
 
         assertTrue(taskRules.requireAssignee)
     }
 
     @Test
-    fun `review의 isDeletable은 false다` () {
-        val taskRules : TaskRules = Review()
+    fun `review의 isDeletable은 false다`() {
+        val taskRules: TaskRules = Review()
 
         assertFalse(taskRules.isDeletable)
     }
 
     @Test
-    fun `review는 todo로 이동하면 예외가 발생한다` () {
-        val taskRules : TaskRules = Review()
+    fun `review는 todo로 이동하면 예외가 발생한다`() {
+        val taskRules: TaskRules = Review()
 
         assertThrows(IllegalStateException::class.java) {
             taskRules.moveTo(TaskStatus.TO_DO)
@@ -106,43 +105,43 @@ class TaskRulesTest {
     }
 
     @Test
-    fun `review는 inProgress로 이동할 수 있다` () {
-        val taskRules : TaskRules = Review()
+    fun `review는 inProgress로 이동할 수 있다`() {
+        val taskRules: TaskRules = Review()
 
         assertTrue(taskRules.moveTo(TaskStatus.IN_PROGRESS) is InProgress)
     }
 
     @Test
-    fun `review는 done으로 이동할 수 있다` () {
-        val taskRules : TaskRules = Review()
+    fun `review는 done으로 이동할 수 있다`() {
+        val taskRules: TaskRules = Review()
 
         assertTrue(taskRules.moveTo(TaskStatus.DONE) is Done)
     }
 
     @Test
-    fun `review의 requireAssignee는 true다` () {
-        val taskRules : TaskRules = Review()
+    fun `review의 requireAssignee는 true다`() {
+        val taskRules: TaskRules = Review()
 
         assertTrue(taskRules.requireAssignee)
     }
 
     @Test
-    fun `done의 isDeletable은 false다` () {
-        val taskRules : TaskRules = Done()
+    fun `done의 isDeletable은 false다`() {
+        val taskRules: TaskRules = Done()
 
         assertFalse(taskRules.isDeletable)
     }
 
     @Test
-    fun `done은 todo로 이동할 수 있다` () {
-        val taskRules : TaskRules = Done()
+    fun `done은 todo로 이동할 수 있다`() {
+        val taskRules: TaskRules = Done()
 
         assertTrue(taskRules.moveTo(TaskStatus.TO_DO) is Todo)
     }
 
     @Test
     fun `done은 inProgress로 이동하면 예외가 발생한다`() {
-        val taskRules : TaskRules = Done()
+        val taskRules: TaskRules = Done()
 
         assertThrows(IllegalStateException::class.java) {
             taskRules.moveTo(TaskStatus.IN_PROGRESS)
@@ -151,7 +150,7 @@ class TaskRulesTest {
 
     @Test
     fun `done은 review로 이동하면 예외가 발생한다`() {
-        val taskRules : TaskRules = Done()
+        val taskRules: TaskRules = Done()
 
         assertThrows(IllegalStateException::class.java) {
             taskRules.moveTo(TaskStatus.REVIEW)
@@ -159,8 +158,8 @@ class TaskRulesTest {
     }
 
     @Test
-    fun `Done의 requireAssignee는 true다` () {
-        val taskRules : TaskRules = Done()
+    fun `Done의 requireAssignee는 true다`() {
+        val taskRules: TaskRules = Done()
 
         assertTrue(taskRules.requireAssignee)
     }
